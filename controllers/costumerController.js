71abb7205e7cd2d53cdf4c4f97dca2d1049b0adf
Vinicius Leal Alves <leealalves@hotmail.com>
2024-05-23 22:24:05 -0300

    feature: added new costumer controllers for login and logout

--- controllers/costumerController.js
+++ controllers/costumerController.js
@@ -1,70 +1,101 @@
 import CostumerServices from "../services/CostumerService.js"
+import bcrypt from "bcrypt"
 import { ObjectId } from "mongodb"
+import utils from "../utils/utils.js"
 
-const getAllCostumers = async (req, res) => {
-    try {
-        const costumers = await CostumerServices.SelectAll()
-        res.status(200).json({ costumers : costumers })
-    } catch(err){
-        console.log(err)
-        res.status(500).json({ err : 'Erro interno do servidor'})
-    }
-}
-
-const createCostumer = async (req, res) => {
-    try {
-        const data = req.body
-        await CostumerServices.Create(data)
-        res.sendStatus(201)
-    } catch(err) {
-        console.log(err)
-        res.status(500).json({ err : 'Erro interno do servidor'})
-    }
-}
-
-const deleteCostumer = async (req, res) => {
-    try {  
-        if(ObjectId.isValid(req.params.id)){
-            const id = req.params.id
-            await CostumerServices.Delete(id)
-            res.sendStatus(204)
+export default {
+    async getAllCostumers(req, res) {
+        try {
+            const costumers = await CostumerServices.SelectAll()
+            res.status(200).json({ costumers : costumers })
+        } catch(err){
+            console.log(err)
+            res.status(500).json({ err : 'Erro interno do servidor'})
         }
-    } catch(err){
-        console.log(err)
-        res.status(500).json({ err : 'Erro interno do servidor'})
-    }
-}
-
-const updateCostumer = async (req, res) => {
-    try {
-        if (ObjectId.isValid(req.params.id)){
-            const id = req.params.id
+    },    
+    async createCostumer(req, res) {
+        try {
             const data = req.body
-            await CostumerServices.Update(id, data)
-            res.sendStatus(200)
+            await CostumerServices.Create(data)
+            res.sendStatus(201)
+        } catch(err) {
+            console.log(err)
+            res.status(500).json({ err : 'Erro interno do servidor'})
         }
-        else {res.sendStatus(400)}
-    } catch(err){
-        console.log(err)
-        res.status(500).json({ err : 'Erro interno do servidor'})
+    },    
+    async deleteCostumer(req, res) {
+        try {  
+            if(ObjectId.isValid(req.params.id)){
+                const id = req.params.id
+                await CostumerServices.Delete(id)
+                res.sendStatus(204)
+            }
+        } catch(err){
+            console.log(err)
+            res.status(500).json({ err : 'Erro interno do servidor'})
+        }
+    },    
+    async updateCostumer(req, res) {
+        try {
+            if (ObjectId.isValid(req.params.id)){
+                const id = req.params.id
+                const data = req.body
+                await CostumerServices.Update(id, data)
+                res.sendStatus(200)
+            }
+            else {res.sendStatus(400)}
+        } catch(err){
+            console.log(err)
+            res.status(500).json({ err : 'Erro interno do servidor'})
+        }
+    },
+    async getOneCostumer(req, res) {
+        try {
+            if (ObjectId.isValid(req.params.id)){
+                const id = req.params.id
+                const costumer = await CostumerServices.SelectOne(id)
+                if(!costumer)
+                    res.sendStatus(404)
+                else
+                    res.status(200).json({ costumer })
+            }
+            else {res.sendStatus(400)}
+        } catch(err){
+            console.log(err)
+            res.status(500).json({ err : 'Erro interno do servidor'})
+        }
+    },
+    async loginCostumer(req, res) {
+        const {costumerEmail, costumerPassword} = req.body
+
+        try {
+            const checkUserExists = await CostumerServices.SelectOneByEmail(costumerEmail)
+    
+            if(!checkUserExists) {
+                return res.status(401).send({err:'Usuário não encontrado.'})
+            }
+        
+            if(!await bcrypt.compare(costumerPassword, checkUserExists.costumerPassword)) {
+                return res.status(401).send({err:'Senha inválida.'})
+            }            
+            
+            res.send({
+                ok: true,
+                message: "Usuário autenticado com sucesso.",
+                token: utils.generateToken(checkUserExists)
+            })
+    
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send({err: "Erro interno ao tentar logar."})
+        }
+    },
+    async logoutCostumer(req, res) {
+        req.session.destroy((err) => {
+          if (err) {
+            return res.status(500).send({err: 'Erro interno ao tentar deslogar.'});
+          }
+          res.sendStatus(204);
+        });
     }
 }
-
-const getOneCostumer = async(req, res) => {
-    try {
-        if (ObjectId.isValid(req.params.id)){
-            const id = req.params.id
-            const costumer = await CostumerServices.SelectOne(id)
-            if(!costumer)
-                res.sendStatus(404)
-            else
-                res.status(200).json({ costumer })
-        }
-        else {res.sendStatus(400)}
-    } catch(err){
-        console.log(err)
-        res.status(500).json({ err : 'Erro interno do servidor'})
-    }
-}
-
-export default {getAllCostumers, createCostumer, deleteCostumer, updateCostumer, getOneCostumer}
